b269e9a3a3306e5a3e0daa8eddab540c0c98203c
tuomas@humanyze.com <Tuomas Jaanu>
2016-10-31 21:27:32 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-templatemailer',
-      version='0.0.4',
+      version='0.0.5',
       description='Django component that is used to send emails using template engine',
       author='Tuomas Jaanu',
       author_email='tuomas@jaa.nu',
